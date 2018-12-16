004eb1de4a34d91b1774df8de7b7d3a091377e30
Jason Johnson <jasonmjohnson1@gmail.com>
2018-12-16 03:53:16 -0500

    Update authzseclib.h

--- src/authzseclib.h
+++ src/authzseclib.h
@@ -1,18 +1,26 @@
 #ifndef AUTHZ_SECLIB
 #define AUTHZ_SECLIB
 #include <AclAPI.h>
+#include <lmcons.h>
 #include <iostream>
 #include <string>
 #include <vector>
+#include <tuple>
 #include <algorithm>
 #include "sqlite3.h"
 #pragma comment(lib, "sqlite3.lib")
 
+typedef struct _generic_ace {
+	ACE_HEADER Header;
+	ACCESS_MASK Mask;
+	DWORD SidStart;
+} generic_ace;
+
 typedef enum _security_policy
 {
 	restricted = 1,
-	read_only,
-	read_write,
+	read_only = GENERIC_READ,
+	read_write = GENERIC_READ | GENERIC_WRITE,
 	execute,
 	standard,
 	administrator,
@@ -34,19 +42,22 @@ class auth_zone
 public:
 	std::wstring name;
 
-	auth_zone(const std::wstring& security_zone, security_policy zone_rules);
+	auth_zone(const std::wstring& trustee_name, const std::wstring& security_zone, security_policy zone_rules);
 	void add_location(const std::wstring& location, bool accessibility);
 	void free_location(const std::wstring& location);
 
 private:
 	std::vector<std::wstring> zone_locations;
 	std::vector<std::wstring> exception_directories;
+	std::vector<std::wstring> standard_sids;
 	security_policy zone_accessibility;
 	trustee_member member;
 
-	void set_control_attribute(const std::wstring& location, PACL access_control_list, PACL access_control_list_old, bool accessibility);
+	void set_control_attribute(const std::wstring& location, PACL access_control_list_old, bool accessibility);
+	PACL renew_access(const std::wstring& location, PACL acl);
 	inline std::vector<std::pair<std::wstring, DWORD>> enumerate_explicit_controls(EXPLICIT_ACCESSW* entries, ULONG entry_count);
-	void database_store_permission_set(const std::vector<std::pair<std::wstring, DWORD>>& explicit_table);
+	void database_store_permission_set(const std::wstring& location, const std::vector<std::pair<std::wstring, DWORD>>& explicit_table);
+
 	void save_permissions(const std::wstring& location, PACL access_control_list);
 };
 
