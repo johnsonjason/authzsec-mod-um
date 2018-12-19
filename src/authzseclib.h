c3ab51728c0cb13719a20f085485bd299c919cb7
Jason Johnson <jasonmjohnson1@gmail.com>
2018-12-19 00:52:15 -0500

    Update authzseclib.h

--- src/authzseclib.h
+++ src/authzseclib.h
@@ -10,18 +10,20 @@
 #include "sqlite3.h"
 #pragma comment(lib, "sqlite3.lib")
 
-typedef struct _generic_ace {
+typedef struct _GENERIC_ACE {
 	ACE_HEADER Header;
 	ACCESS_MASK Mask;
 	DWORD SidStart;
-} generic_ace;
+} ACCESS_GENERIC_ACE;
 
 typedef enum _security_policy
 {
-	restricted = 1,
+	restricted = NULL,
+	full_control = GENERIC_ALL,
 	read_only = GENERIC_READ,
 	read_write = GENERIC_READ | GENERIC_WRITE,
-	execute,
+	write = GENERIC_WRITE,
+	execute = GENERIC_EXECUTE,
 	standard,
 	administrator,
 } security_policy;
@@ -41,24 +43,22 @@ class auth_zone
 {
 public:
 	std::wstring name;
+	std::vector<std::wstring> standard_sids;
 
 	auth_zone(const std::wstring& trustee_name, const std::wstring& security_zone, security_policy zone_rules);
-	void add_location(const std::wstring& location, bool accessibility);
+	void add_location(const std::wstring& location);
 	void free_location(const std::wstring& location);
 
 private:
 	std::vector<std::wstring> zone_locations;
 	std::vector<std::wstring> exception_directories;
-	std::vector<std::wstring> standard_sids;
 	security_policy zone_accessibility;
 	trustee_member member;
 
-	void set_control_attribute(const std::wstring& location, PACL access_control_list_old, bool accessibility);
-	PACL renew_access(const std::wstring& location, PACL acl);
-	inline std::vector<std::pair<std::wstring, DWORD>> enumerate_explicit_controls(EXPLICIT_ACCESSW* entries, ULONG entry_count);
-	void database_store_permission_set(const std::wstring& location, const std::vector<std::pair<std::wstring, DWORD>>& explicit_table);
-
-	void save_permissions(const std::wstring& location, PACL access_control_list);
+	void set_control_attribute(const std::wstring& location, PACL access_control_list_old);
+	bool lease_access(const std::wstring& location, PACL acl);
+	void database_store_permission_set(const std::vector<std::tuple<char, std::wstring, std::wstring, size_t, DWORD>>& entries);
+	bool is_primary_sid(const std::wstring& sid_username);
 };
 
 #endif
