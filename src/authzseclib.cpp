9233405bf40dea2820ba0c37926b6b9ec541a831
Jason Johnson <jasonmjohnson1@gmail.com>
2018-12-19 00:53:10 -0500

    Update authzseclib.cpp

--- src/authzseclib.cpp
+++ src/authzseclib.cpp
@@ -1,6 +1,3 @@
-// authzseclib.cpp : Defines the exported functions for the DLL application.
-//
-
 #include "stdafx.h"
 #include "authzseclib.h"
 
@@ -13,7 +10,7 @@ auth_zone::auth_zone(const std::wstring& trustee_name, const std::wstring& secur
 }
 
 
-void auth_zone::add_location(const std::wstring& location, bool accessibility)
+void auth_zone::add_location(const std::wstring& location)
 {
 	PACL access_control_list_old = NULL;
 
@@ -22,7 +19,7 @@ void auth_zone::add_location(const std::wstring& location, bool accessibility)
 
 	if (!access_control_list_old)
 	{
-		std::cout << "Error getting named security information." << std::endl;
+		std::cerr << "Error getting named security information.\n";
 		return;
 	}
 
@@ -31,7 +28,8 @@ void auth_zone::add_location(const std::wstring& location, bool accessibility)
 	case read_only:
 	case read_write:
 	case execute:
-		set_control_attribute(location, access_control_list_old, accessibility);
+	case full_control:
+		set_control_attribute(location, access_control_list_old);
 	case restricted:
 		break;
 	case standard:
@@ -41,51 +39,103 @@ void auth_zone::add_location(const std::wstring& location, bool accessibility)
 	}
 
 	this->zone_locations.push_back(location);
-	save_permissions(location, access_control_list_old);
 }
 
-PACL auth_zone::renew_access(const std::wstring& location, PACL acl)
+bool auth_zone::is_primary_sid(const std::wstring& sid_username)
+{
+	for (const std::wstring& sid : this->standard_sids)
+	{
+		if (sid_username == sid)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+static DWORD reverse_permission_mask_deny(security_policy policy)
+{
+	switch (policy)
+	{
+	case security_policy::read_only:
+		return GENERIC_WRITE | GENERIC_EXECUTE;
+	case security_policy::write:
+		return GENERIC_READ | GENERIC_EXECUTE;
+	case security_policy::execute:
+		return GENERIC_WRITE;
+	}
+
+	return NULL;
+}
+
+bool auth_zone::lease_access(const std::wstring& location, PACL acl)
 {
 	// access=allowed/denied
 	// <access:Location:SIDString:ACEIndex:Permission>
 	std::vector<std::tuple<char, std::wstring, std::wstring, size_t, DWORD>> entries;
-
 	DWORD user_length = UNLEN;
-	DWORD domain_length = DNLEN;
-
+	DWORD domain_length = UNLEN;
 	WCHAR* username = new TCHAR[user_length];
 	WCHAR* domain = new TCHAR[domain_length];
 
 	for (size_t acindex = 0; acindex < acl->AceCount; acindex++)
 	{
-		generic_ace* ace;
+		/* Redefining the lengths because the lookup routine will modify them */
+		user_length = UNLEN;
+		domain_length = UNLEN;
+		ACCESS_GENERIC_ACE* ace;
+
 		GetAce(acl, acindex, reinterpret_cast<PVOID*>(&ace));
 		if (ace->Header.AceType == ACCESS_ALLOWED_ACE_TYPE)
 		{
-			SID *sid;
+			SID* sid = reinterpret_cast<SID*>(&reinterpret_cast<ACCESS_GENERIC_ACE*>(ace)->SidStart);
 			SID_NAME_USE sid_type;
 			
 			if (LookupAccountSidW(NULL, sid, username, &user_length, domain, &domain_length, &sid_type))
 			{
-				entries.push_back(std::make_tuple('a', location, std::wstring(username), acindex, ace->Mask));
+				if (!is_primary_sid(std::wstring(username)))
+				{
+					entries.push_back(std::make_tuple('a', location, std::wstring(username), acindex, ace->Mask));
+					ace->Mask = this->zone_accessibility;
+				}
+			}
+			else
+			{
+				return false;
 			}
 		}
+
 		else if (ace->Header.AceType == ACCESS_DENIED_ACE_TYPE)
 		{
-			SID *sid;
+			SID* sid = reinterpret_cast<SID*>(&reinterpret_cast<ACCESS_GENERIC_ACE*>(ace)->SidStart);
 			SID_NAME_USE sid_type;
 			if (LookupAccountSidW(NULL, sid, username, &user_length, domain, &domain_length, &sid_type))
 			{
-				entries.push_back(std::make_tuple('d', location, std::wstring(username), acindex, ace->Mask));
+				if (!is_primary_sid(std::wstring(username)))
+				{
+					entries.push_back(std::make_tuple('d', location, std::wstring(username), acindex, ace->Mask));
+					ace->Mask = reverse_permission_mask_deny(this->zone_accessibility);
+				}
 			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return false;
 		}
 	}
 
 	delete[] username;
 	delete[] domain;
+	database_store_permission_set(entries);
+	SetNamedSecurityInfoW(const_cast<LPWSTR>(location.c_str()), SE_FILE_OBJECT, DACL_SECURITY_INFORMATION, NULL, NULL, acl, NULL);
+	return true;
 }
 
-void auth_zone::set_control_attribute(const std::wstring& location, PACL access_control_list_old, bool accessibility)
+void auth_zone::set_control_attribute(const std::wstring& location, PACL access_control_list_old)
 {
 	switch (this->zone_accessibility)
 	{
@@ -96,79 +146,71 @@ void auth_zone::set_control_attribute(const std::wstring& location, PACL access_
 	case administrator:
 		break;
 	default:
-		PACL access_control_list = renew_access(location, access_control_list_old);
-		SetNamedSecurityInfoW(const_cast<LPWSTR>(location.c_str()), SE_FILE_OBJECT, DACL_SECURITY_INFORMATION, NULL, NULL,
-			access_control_list, NULL);
+		lease_access(location, access_control_list_old);
 	}
-}
-
-
-inline std::vector<std::pair<std::wstring, DWORD>> auth_zone::enumerate_explicit_controls(EXPLICIT_ACCESSW* entries, ULONG entry_count)
-{
-	std::vector<std::pair<std::wstring, DWORD>> ac_pair;
-	for (size_t i = 0; i < entry_count; i++)
-	{
-		ac_pair.push_back({ std::wstring(entries[i].Trustee.ptstrName), entries[i].grfAccessPermissions });
-	}
-	return ac_pair;
 }
 
 static int sqlite_exec_callback(void* reserved, int argc, char** argv, char** az_colname)
 {
-	for (int i = 0; i < argc; i++)
+	for (size_t i = 0; i < argc; i++)
 	{
 		std::cout << az_colname << (argv[i] ? argv[i] : "NULL") << '\n';
 	}
 	return 0;
 }
 
-
-static std::vector<std::string> enumerate_set_controls(sqlite3* database, const std::wstring& object_name, const std::vector<std::pair<std::wstring, DWORD>>& explicit_table)
+static std::string get_mask(DWORD mask)
 {
-	std::string ascii_objname(object_name.begin(), object_name.end());
-	std::vector<std::string> queries;
-	for (auto entry : explicit_table)
-	{
-		queries.push_back("INSERT INTO '" + ascii_objname + "'(trustee, permission) " \
-			"VALUES ( '" + std::string(entry.first.begin(), entry.first.end()) + "', " + std::to_string(entry.second) + " );");
+	std::string permission;
+	if (FILE_READ_DATA & mask) {
+		permission += "READ_";
 	}
+	if (FILE_WRITE_DATA & mask) {
+		permission += "WRITE_";
+	}
+	if (FILE_EXECUTE & mask) {
+		permission += "EXECUTE_";
+	}
+	return permission;
+}
+
+
+static std::vector<std::string> enumerate_set_controls(const std::string& objname, const std::vector<std::tuple<char, std::wstring, std::wstring, size_t, DWORD>>& entries)
+{
+	std::vector<std::string> queries;
+
+	for (auto entry : entries)
+	{
+		std::string location(std::get<1>(entry).begin(), std::get<1>(entry).end());
+		std::string sid_string(std::get<2>(entry).begin(), std::get<2>(entry).end());
+		std::string permission = get_mask(std::get<4>(entry));
+
+		queries.push_back("INSERT INTO '" + objname +
+			"'(Access, Location, SIDString, ACEIndex, Permission) " \
+			"VALUES ('" + std::get<0>(entry) + "', '" + location + "', '" + sid_string + "', " + std::to_string(std::get<3>(entry)) + ", '" + permission + "'" + ")");
+	}
+
 	return queries;
 }
 
-void auth_zone::database_store_permission_set(const std::wstring& location, const std::vector<std::pair<std::wstring, DWORD>>& explicit_table)
+void auth_zone::database_store_permission_set(const std::vector<std::tuple<char, std::wstring, std::wstring, size_t, DWORD>>& entries)
 {
 	sqlite3* database;
 	char* error_msg = NULL;
 	int result = 0;
 
-	std::string authzsec_table = "CREATE TABLE IF NOT EXISTS '" + std::string(location.begin(), location.end()) + "'(trustee TEXT, permission INT)";
+	std::string authzsec_table = "CREATE TABLE IF NOT EXISTS '" + std::string(this->name.begin(), this->name.end()) + "'(Access TEXT, Location TEXT, SIDString TEXT, ACEIndex INT," + 
+		"Permission TEXT)";
 	
 	result = sqlite3_open("authzsec_store.db", &database);
 	result = sqlite3_exec(database, authzsec_table.c_str(), sqlite_exec_callback, 0, &error_msg);
-	std::vector<std::string> queries = enumerate_set_controls(database, location, explicit_table);
+	std::vector<std::string> queries = enumerate_set_controls(std::string(this->name.begin(), this->name.end()), entries);
 
 	for (std::string query : queries)
 	{
+		std::cout << query.c_str() << std::endl;
 		sqlite3_exec(database, query.c_str(), sqlite_exec_callback, 0, &error_msg);
 	}
 
 	result = sqlite3_close(database);
-}
-
-void auth_zone::save_permissions(const std::wstring& location, PACL access_control_list)
-{
-	if (std::find(this->zone_locations.begin(), this->zone_locations.end(), location) == this->zone_locations.end())
-	{
-		throw authzsec_exception::zone_not_found;
-	}
-
-	ULONG entry_count;
-	EXPLICIT_ACCESSW* entries;
-	std::cout << GetExplicitEntriesFromAclW(access_control_list, &entry_count, &entries) << std::endl;
-
-	if (entries != NULL)
-	{
-		database_store_permission_set(location, enumerate_explicit_controls(entries, entry_count));
-		LocalFree(entries);
-	}
 }
